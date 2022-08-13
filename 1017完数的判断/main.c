bafa6ec3ee253132f78583ca96ff748d04fb9f2f
Sebastian <96730821+Sebastian0357@users.noreply.github.com>
2022-08-13 18:38:02 +0800

    Add files via upload

---
+++ 1017完数的判断/main.c
@@ -0,0 +1,55 @@
+/*
+题目描述
+一个数如果恰好等于不包含它本身所有因子之和，这个数就称为"完数"。 
+例如，6的因子为1、2、3，而6=1+2+3，因此6是"完数"。 编程序找出N之内的所有完数，
+并按下面格式输出其因子
+
+输入格式
+N
+
+输出格式
+? its factors are ? ? ?
+
+样例输入
+1000
+样例输出
+6 its factors are 1 2 3 
+28 its factors are 1 2 4 7 14 
+496 its factors are 1 2 4 8 16 31 62 124 248 
+*/
+#include <stdio.h>
+
+int main()
+{
+	int arr[100],n,i,j,l;
+
+	scanf("%d",&n);
+
+	for(i=1;i<n;i++)
+	{
+		int sum=0,k=0;
+		for(j=1;j<i;j++)
+		{
+			if(i%j==0)
+			{
+				sum+=j;
+				arr[k++]=j;
+				
+			}
+		}
+		if(sum==i)
+		{
+
+			printf("%d its factors are",i);
+			for(l=0;l<k;l++)
+			{
+				printf(" %d",arr[l]);
+			}
+			printf("\n");
+		}
+	}
+	
+
+
+	return 0;
+}
